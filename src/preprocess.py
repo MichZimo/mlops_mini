23b3eacb9b5fe15065716a6af4b1e8bdd684171a
MichZimo <mzim07558@gmail.com>
2026-05-06 21:07:16 +0200

    Fixed plotting logic

--- src/preprocess.py
+++ src/preprocess.py
@@ -15,18 +15,23 @@ def load_data():
     return df[['MedInc', 'Longitude', 'Latitude', 'MedHouseVal']]
 
 def prepare_features(df):
-    X = df_pred[['MedInc', 'Longitude', 'Latitude']] # Features
-    y = df_pred['MedHouseVal'] # Targets
+    X = df[['MedInc', 'Longitude', 'Latitude']] # Features
+    y = df['MedHouseVal'] # Targets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
     return X_train, X_test, y_train, y_test
 
 def create_plot(df, plotting=False, plot_path=None):  # only if needed
-    if !plotting:
-        sns.pairplot(df_pred)
-        if plot_path is None:
-            plot_path.parent.mkdir(parents=True, exist_ok=True)
-            plot_path = plot_path / "data" / "processed" / "cali_housing_pairplot.png"
-        plt.savefig(plot_path)
-        plt.close()
-        mlflow.log_artifact(str(plot_path))
+    if not plotting:
+        return
+
+    if plot_path is None:
+        plot_path = Path("data/processed/cali_housing_pairplot.png")
+    
+    plot_path.parent.mkdir(parents=True, exist_ok=True)
+    
+    sns.pairplot(df)
+    plt.savefig(plot_path)
+    plt.close()
+    
+    mlflow.log_artifact(str(plot_path))
     return
